4465f1efbf41bdff035c9f1d87015b2f842fe27d
kbalubam <kevin.baluba@hotmail.com>
2021-09-29 14:36:12 +0200

    aj

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
+    return view('front.pages.home');
 });
 
 Route::get('/dashboard', function () {
