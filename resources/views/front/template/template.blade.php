b708d26b15411fac3e92718561c3c02e92c384e3
kbalubam <kevin.baluba@hotmail.com>
2021-09-29 14:21:12 +0200

    laravelisation2

--- resources/views/front/template/template.blade.php
+++ resources/views/front/template/template.blade.php
@@ -34,20 +34,7 @@
         ======================================================== -->
       </head>
 <body>
-<main id="main">
 
-
-@include('front.partials.Navbar')
-@include('front.partials.buildBetter')
-@include('front.partials.Voluptatem')
-@include('front.partials.Features')
-@include('front.partials.Services')
-@include('front.partials.Portfolio')
-@include('front.partials.Team')
-@include('front.partials.Contact')
-@include('front.partials.Footer')
-</main><!-- End #main -->
-@include('front.partials.Footer')
 
 
   <!-- Vendor JS Files -->
