d4921f3d4329c6d600aacd1eb50744bc7182c4e8
ElsongMaster <elsongmaster@gmail.com>
2021-09-30 18:13:47 +0200

    MAJ Els

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -11,10 +11,6 @@
     @include('front.partials.Testimonial')
     @include('front.partials.Team')
     @include('front.partials.Contact')
-<<<<<<< HEAD
     
-=======
-
->>>>>>> fdcf633d356bbf4251fca44787f9b115e8e3bc25
 </main><!-- End #main -->
 @include('front.partials.Footer')
